09b2a0bc7e36312ff59430f8b4f0fe8fe522c190
GitSpreed <31727970+GitSpreed@users.noreply.github.com>
2018-04-18 07:05:51 +0800

    Add files via upload

---
+++ homework4/Hit UFO/Assets/ScoreRecorder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreRecorder : MonoBehaviour {
+
+    private int score;
+
+    private Dictionary<Color, int> scoreTable = new Dictionary<Color, int>();
+
+    protected ScoreRecorder() { }
+
+    // Use this for initialization  
+    void Start()
+    {
+        score = 0;
+        scoreTable.Add(Color.red, 1);
+        scoreTable.Add(Color.yellow, 2);
+        scoreTable.Add(Color.blue, 3);
+    }
+
+    public void Record(GameObject disk)
+    {
+        score += scoreTable[disk.GetComponent<DiskData>().color];
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+   
+    public void Reset()
+    {
+        score = 0;
+    }
+}

4a82ccfeee4e3b28f1db255917f467adbe6302f6
GitSpreed <31727970+GitSpreed@users.noreply.github.com>
2018-06-05 23:56:07 +0800

    Add files via upload

---
+++ homework8/Assets/GarenEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarenEvent : MonoBehaviour {
+
+	public delegate void GarenSubject (GameObject self, string message);
+	public static event GarenSubject OnGarenSubjectNotify;
+
+	void JumpTopPoint() {
+		Debug.Log ("Jump to the top point!!!");
+		if (OnGarenSubjectNotify != null)
+			OnGarenSubjectNotify (this.gameObject, "AtTop");
+	}
+}
---
+++ homework8/Assets/HealthPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPanel : MonoBehaviour {
+
+    public float healthPanelOffset = 0.35f;
+    public GUISkin mySkin;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y + healthPanelOffset, transform.position.z);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        GUI.HorizontalSlider(new Rect(screenPos.x - 50, screenPos.y - 30, 100, 100), 50, 0, 100);
+    }
+}
